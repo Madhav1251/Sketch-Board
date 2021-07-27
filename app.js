ed22016eef3872fcb1d1cd13121705a9240be38f
Madhav Aggarwal <madhavaggarwalsng@gmail.com>
2021-07-27 16:36:51 +0530

    Finished Project

---
+++ app.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC_RszMac-2GT95bG8kCD015tI7ynlG7Fc",
+    authDomain: "sketch-board-7179e.firebaseapp.com",
+    projectId: "sketch-board-7179e",
+    storageBucket: "sketch-board-7179e.appspot.com",
+    messagingSenderId: "617529200616",
+    appId: "1:617529200616:web:b88f50c6fe0a15b6929e5d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
